44d5bcb4fbbb78d83f3e3499d3abef4dcac3148e
Nilson Valdés <nigivamu@hotmail.com>
2026-01-30 20:39:28 -0500

    feat: update hst configurtion

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
+        allowedHosts: ['nilson.tech', 'valdes.lat'],
         port: 3000,
         host: true,
         open: true
